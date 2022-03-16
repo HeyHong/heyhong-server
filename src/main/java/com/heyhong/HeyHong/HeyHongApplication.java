1bc278e32193acbbbd83ba44676e8149babc1595
kim_ki_yun <kiyoon9390@gmail.com>
2022-03-16 21:01:52 +0900

    feat:first commit

---
+++ src/main/java/com/heyhong/HeyHong/HeyHongApplication.java
@@ -0,0 +1,13 @@
+package com.heyhong.HeyHong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyHongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyHongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyhong/HeyHong/HeyHongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyhong.HeyHong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyHongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
